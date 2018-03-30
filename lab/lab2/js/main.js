8556389021aa7287f439dee91172d0479d312b5c
sydng <35381098+sydng@users.noreply.github.com>
2018-03-30 13:41:39 -0400

    updates/cleaning up code

--- lab/lab2/js/main.js
+++ lab/lab2/js/main.js
@@ -122,6 +122,8 @@ var dataset;
 var coordinates;
 var routesURL;
 var routes;
+var latitude;
+var longitude;
 
 var state = {
   position: {
@@ -147,6 +149,8 @@ var updatePosition = function(lat, lng, updated) {
   state.position.updated = updated;
   state.position.marker.bindPopup("Latitude:" + lat.toString() + "  " + "Longitude:" + lng.toString()).addTo(map);
   goToOrigin(lat, lng);
+  latitude = lat;
+  longitude = lng;
 };
 
 $(document).ready(function() {
@@ -188,22 +192,22 @@ $(document).ready(function() {
         return coordinateMarkers;
       });
       coordinateMarkers.addTo(map);
+
       routesURL = "https://api.mapbox.com/directions/v5/mapbox/cycling/" +
-          coordinates[0][0] + "," + coordinates[0][1] +
-          ";-74.195,39.958?access_token=pk.eyJ1Ijoic3lkbmciLCJhIjoiY2pmY3JuM2x3M3poYjJ3bzFwZmV4ZHdwdCJ9.H7Kk8jKeATVVDR9qaVxU2Q";
+        coordinates[0][0] + "," + coordinates[0][1] + ";" +
+        longitude + "," + latitude + "?access_token=pk.eyJ1Ijoic3lkbmciLCJhIjoiY2pmY3JuM2x3M3poYjJ3bzFwZmV4ZHdwdCJ9.H7Kk8jKeATVVDR9qaVxU2Q";
       console.log(routesURL);
       $.ajax(routesURL).done(function(nextSteps) {
-      var routes = nextSteps.routes[0].geometry;
-      console.log(routes);
-      var decoded = _.map(decode(routes), function(point) {
-        return [point[1], point[0]];
-      });
-      var lineString = turf.lineString(decoded, {name: 'line 1'});
-      console.log(lineString);
-      L.geoJSON(lineString, {
-        color: "black"
-      }).addTo(map);
-      //console.log(path);
+        var routes = nextSteps.routes[0].geometry;
+        console.log(routes);
+        var decoded = _.map(decode(routes), function(point) {
+          return [point[1], point[0]];
+        });
+        var lineString = turf.lineString(decoded, {name: 'line 1'});
+        console.log(lineString);
+        L.geoJSON(lineString, {
+          color: "black"
+        }).addTo(map);
       });
     });
   });
